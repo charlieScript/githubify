5733442c52b6990884a9e9eb7acf4cb9addbec3b
Peter <ndekepeter2015@gmail.com>
2020-08-17 15:43:55 +0100

    fixed some changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import Chart from 'chart.js'
-var GhPolyglot = require('gh-polyglot');
+import React, { useEffect, useState } from "react";
+import Chart from "chart.js";
+var GhPolyglot = require("gh-polyglot");
 
 function Profile(props) {
   const [data, setData] = useState([]);
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState("");
+  // random colors
+  function randomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let a = Math.floor(Math.random()) + 1;
+    let randomColor = `rgba(${r}, ${g}, ${b}, ${a})`
+    return randomColor;
+  }
   // avatar_url, login, followers, following, public_repos, url, company, location, created_at, name
   const input = props.location.state;
   useEffect(() => {
@@ -13,52 +22,63 @@ function Profile(props) {
       .then((res) => {
         setData(res);
         setDate(res.created_at);
-        let x = new Date(res.created_at)
+        let x = new Date(res.created_at);
         return x;
       })
       .then((i) => {
-        const option = { year: 'numeric', month: 'long', day: 'numeric' };
-        setDate(i.toLocaleString(undefined, option))
+        const option = { year: "numeric", month: "long", day: "numeric" };
+        setDate(i.toLocaleString(undefined, option));
       })
       .catch((err) => console.log(err));
+    // random background color
     const me = new GhPolyglot(input);
-    var ctx = document.getElementById('myChart');
+    var ctx = document.getElementById("myChart");
     me.userStats(function (err, stats) {
-      console.log(stats)
-      let lab = stats.map(i => i.value)
-      console.log(lab)
+      let newStats = [];
+      if (stats !== undefined) {
+        newStats = stats;
+        var arrRandomColor = []
+        for (let i = 0; i < stats.length; i++) {
+          arrRandomColor.push(randomColor())
+        }
+      } else {
+        console.log("empty data");
+      }
       var myChart = new Chart(ctx, {
-        type: 'doughnut',
+        type: "doughnut",
         data: {
-          labels: stats.map((i) => i.label),
+          labels: newStats.length !== 0 ? newStats.map((i) => i.label) : null,
           datasets: [
             {
-              label: '# of Votes',
-              data: stats.map((i) => i.value),
-              // backgroundColor: [
-              //   'rgba(255, 99, 132, 0.2)',
-              //   'rgba(54, 162, 235, 0.2)',
-              //   'rgba(255, 206, 86, 0.2)',
-              //   'rgba(75, 192, 192, 0.2)',
-              // ],
-              // borderColor: [
-              //   'rgba(255, 99, 132, 1)',
-              //   'rgba(54, 162, 235, 1)',
-              //   'rgba(255, 206, 86, 1)',
-              //   'rgba(75, 192, 192, 1)',
-              //   'rgba(153, 102, 255, 1)',
-              //   'rgba(255, 159, 64, 1)',
-              // ],
+              label: "# of Votes",
+              data: newStats.length !== 0 ? newStats.map((i) => i.value) : null,
+              backgroundColor: arrRandomColor,
               borderWidth: 1,
             },
           ],
         },
       });
     });
+    let repoStars = []
+    let repoSize = []
+    let repoForks = []
     me.getAllRepos(function (err, stats) {
       // language, url
-      console.log(err || stats);
+      // console.log(err || stats);
+      repoStars = stats.sort((a, b) => a.starsgazer_count - b.starsgazer_count);
+      repoSize = stats.sort((a, b) => b.size - a.size);
+      repoForks = stats.sort((a, b) => b.forks_count - a.forks_count);
+      // console.log(repoStars);
+      console.log(repoForks);
     });
+    fetch('https://api.github.com/rate_limit')
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+    // fetch(`https://api.github.com/users/${input}/repos`)
+    // .then(res => res.json())
+    // .then(res => console.log(res))
+    // .catch(err => console.log(err))
   }, [input]);
   return (
     <div className="profile">
@@ -82,8 +102,8 @@ function Profile(props) {
           <p>Following</p>
         </div>
       </div>
-
-      <canvas id="myChart" width="400" height="400"></canvas>
+      <canvas id="myChart" width="100" height="100"></canvas>
+      
     </div>
   );
 }
