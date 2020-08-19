d18b5924f8ac40b8d876231ec8ec6f68baab4c59
Peter <ndekepeter2015@gmail.com>
2020-08-19 14:47:17 +0100

    build

---
+++ build/precache-manifest.a027d3e32dc940ecf5c163e2b55f3296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd4781028ad952e841e032211e9ae01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22701cde1bf644351807",
+    "url": "/static/css/main.2a872918.chunk.css"
+  },
+  {
+    "revision": "0596ad433f237d154bd4",
+    "url": "/static/js/2.1142e41b.chunk.js"
+  },
+  {
+    "revision": "8d651a66eecf3881c30a3d87e1cb3ab7",
+    "url": "/static/js/2.1142e41b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22701cde1bf644351807",
+    "url": "/static/js/main.698d0b95.chunk.js"
+  },
+  {
+    "revision": "df02d491b65003c67a5a",
+    "url": "/static/js/runtime-main.7cf6418b.js"
+  }
+]);
